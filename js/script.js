339dbf457e1784de85d9836262681308b9bbac27
nuryyy <108506953+nuryyy@users.noreply.github.com>
2022-07-02 23:57:47 +0700

    Duration typewrite --

--- js/script.js
+++ js/script.js
@@ -28,6 +28,6 @@ function typeWrite() {
     if (i < message.length) {
         textArea.innerHTML += message.charAt(i);
         i++;
-        setTimeout(typeWrite, 90);
+        setTimeout(typeWrite, 65);
     }
 }
